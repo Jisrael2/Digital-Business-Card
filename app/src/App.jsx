7c4e9bc71cc5fdba4c42cf981a67315f7bee2ff1
Israel Jesusetemi <jesusetemiisrael@gmail.com>
2024-08-26 16:33:47 +0100

    initial commit

---
+++ app/src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className= "body--frame">
+      <div className= "back-of-card">
+      <div className="black--section" ></div>
+      <img className="image-logo" src=".\src\assets\images\image-logo.png"/>
+      <img className="text--logo" src=".\src\assets\images\text-logo.png"/>
+      <img className="golden--shape" src=".\src\assets\images\golden-part.svg" />
+      </div>
+      <div className="front-of-card">
+        <div>
+          <h1 className="motto-text" >Be a Hero</h1>
+          <span>
+            <img className="image-logo-small" src=".\src\assets\images\image-logo.png"/>
+            <img className="text-logo-small" src=".\src\assets\images\text-logo.png"/>
+          </span>
+
+        </div>
+        <img className="black--shape" src=".\src\assets\images\black-shape.svg" />
+        <span className="owner-info">
+          <big className="black-name">   Jesusetemi</big>
+          <big className="yellow-name">    Akanji</big>
+          <small className="owner-position"> Developer</small>
+        </span>
+
+        <div>
+          <span>
+            <img className="icon--image" src=".\src\assets\images\phone-icon.svg" />
+            <small>    2348076169512</small>
+          </span>
+          <br/>
+          <span>
+            <img className="icon--image" src=".\src\assets\images\email-icon.svg" />
+            <small>    jesusetemiisrael@gmail.com</small>
+          </span>
+          <br/>
+          <span>
+            <img className="icon--image" src=".\src\assets\images\location-icon.svg" />
+            <small>    16, Aiyedun, Bariga, Lagos, Nigeria.</small>
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
